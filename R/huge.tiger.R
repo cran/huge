63757d7affb65c2e8e91f0e86855419b8e18c9f7
Haoming Jiang <jianghm.ustc@gmail.com>
2019-02-22 06:00:03 +0000

    version 1.3.0

---
+++ R/huge.tiger.R
@@ -0,0 +1,94 @@
+#-----------------------------------------------------------------------#
+# Package: High-dimensional Undirected Graph Estimation                 #
+# huge.tiger(): Tuning-insensitive graph estimation                     #
+#-----------------------------------------------------------------------#
+
+## Main function
+huge.tiger = function(x, lambda = NULL, nlambda = NULL, lambda.min.ratio = NULL, sym = "or", verbose = TRUE)
+{
+	gcinfo(FALSE)
+	n = nrow(x);
+	d = ncol(x);
+	fit = list()
+	fit$cov.input = isSymmetric(x);
+	if(fit$cov.input)
+	{
+		if(verbose) cat("The input is identified as the covariance matrix.\n")
+		S = cov2cor(x);
+	}
+	if(!fit$cov.input)
+	{
+		x = scale(x)
+		S = cor(x)
+	}
+
+	gc()
+
+	if(!is.null(lambda)) nlambda = length(lambda)
+	if(is.null(lambda))
+	{
+		if(is.null(nlambda))
+			nlambda = 10
+		if(is.null(lambda.min.ratio))
+			lambda.min.ratio = 0.1
+		lambda.max = max(max(S-diag(d)),-min(S-diag(d)))
+		lambda.min = lambda.min.ratio*lambda.max
+		lambda = exp(seq(log(lambda.max), log(lambda.min), length = nlambda))
+		rm(lambda.max,lambda.min,lambda.min.ratio)
+		gc()
+	}
+	maxdf = min(d,n);
+
+	if(verbose)
+	{
+	  cat("Conducting graph estimation through a tunning-insensitive approach(tiger)....")
+	  flush.console()
+	}
+	fit$idx_mat = NULL
+	out = .Call("_huge_SPMBgraphsqrt", x, lambda, nlambda, d, as.double(rep(0,d*maxdf*nlambda)),as.integer(rep(0,d+1)), as.integer(rep(0,d*maxdf*nlambda)))
+	for(i in 1:d)
+	{
+	  if(out$col_cnz[i+1]>out$col_cnz[i])
+	  {
+	    idx.tmp = (out$col_cnz[i]+1):out$col_cnz[i+1]
+	    ord = order(out$row_idx[idx.tmp])
+	    out$row_idx[idx.tmp] = out$row_idx[ord + out$col_cnz[i]]
+	    out$x[idx.tmp] = out$x[ord + out$col_cnz[i]]
+	  }
+	}
+
+
+  G = new("dgCMatrix", Dim = as.integer(c(d*nlambda,d)), x = as.vector(out$x[1:out$col_cnz[d+1]]),p = as.integer(out$col_cnz), i = as.integer(out$row_idx[1:out$col_cnz[d+1]]))
+
+	fit$beta = list()
+	fit$path = list()
+	fit$df = matrix(0,d,nlambda)
+	fit$rss = matrix(0,d,nlambda)
+	fit$sparsity = rep(0,nlambda)
+	for(i in 1:nlambda)
+	{
+		fit$beta[[i]] = G[((i-1)*d+1):(i*d),]
+		fit$path[[i]] = abs(fit$beta[[i]])
+		fit$df[,i] = apply(sign(fit$path[[i]]),2,sum)
+
+		if(sym == "or")
+			fit$path[[i]] = sign(fit$path[[i]] + t(as.matrix(fit$path[[i]])))
+		if(sym == "and")
+			fit$path[[i]] = sign(fit$path[[i]] * t(fit$path[[i]]))
+		fit$sparsity[i] = sum(fit$path[[i]])/d/(d-1)
+	}
+	fit$icov = out$icov
+ 	rm(x, G, out)
+
+ 	fit$lambda = lambda
+
+	if(verbose)
+ 	{
+ 		cat("done\n")
+      flush.console()
+  }
+
+ 	rm(verbose,nlambda)
+ 	gc()
+ 	return(fit)
+}
--- src/RIC.cpp
+++ src/RIC.cpp
@@ -1,33 +1,29 @@
-#include "R.h"
 #include "math.h"
+#include <Rcpp.h>
+using namespace Rcpp;
 
-void RIC(double *X, int *dd, int *nn, int *r, int *nr, double *lambda_opt)
+//[[Rcpp::export]]
+double RIC(NumericMatrix &X, int d, int n, NumericVector &r, int t)
 {
-	int d,n;
-	int i,j,k,m;
-    int t;
-    int tmp_r,tmp_i,tmp_j;
-	
-	d = dd[0];
-	n = nn[0];
-    t = nr[0];
-    
-	double lambda_min,lambda_max,tmp;
-	
-	lambda_min = 99999999;
-	
+    int i,j,k,m;
+    int tmp_r;
+
+    double lambda_min,lambda_max,tmp;
+
+    lambda_min = 99999999;
+
     for(i=0;i<t;i++)
     {
         tmp_r = r[i];
         lambda_max = 0;
-        for(j=0;j<d;j++)
+        for(j=0;j<d;j++) {
             for(k=0;k<j;k++)
             {
                 tmp = 0;
                 for(m=0;m<(n-tmp_r);m++)
-                    tmp = tmp + X[j*n+m+tmp_r]*X[k*n+m];
+                    tmp = tmp + X(m+tmp_r, j)*X(m,k);
                 for(m=(n-tmp_r);m<n;m++)
-                    tmp = tmp + X[j*n+m-(n-tmp_r)]*X[k*n+m];
+                    tmp = tmp + X(m-(n-tmp_r), j)*X(m,k);
                 tmp = fabs(tmp);
                 if(tmp>lambda_max)
                     lambda_max = tmp;
@@ -36,15 +32,16 @@ void RIC(double *X, int *dd, int *nn, int *r, int *nr, double *lambda_opt)
             {
                 tmp = 0;
                 for(m=0;m<(n-tmp_r);m++)
-                    tmp = tmp + X[j*n+m+tmp_r]*X[k*n+m];
+                    tmp = tmp + X(m+tmp_r, j)*X(m,k);
                 for(m=(n-tmp_r);m<n;m++)
-                    tmp = tmp + X[j*n+m-(n-tmp_r)]*X[k*n+m];
+                    tmp = tmp + X(m-(n-tmp_r), j)*X(m,k);
                 tmp = fabs(tmp);
                 if(tmp>lambda_max)
                     lambda_max = tmp;
             }
+        }
         if(lambda_max<lambda_min)
             lambda_min = lambda_max;
     }
-    lambda_opt[0] = lambda_min;
+    return lambda_min;
 }
